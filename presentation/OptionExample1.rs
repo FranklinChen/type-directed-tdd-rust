71e91354c2cd1b782265d0757f43e4f3923ec00b
Franklin Chen <franklinchen@franklinchen.com>
2014-07-21 00:08:48 -0400

    Fix typo in code slide

--- presentation/OptionExample1.rs
+++ presentation/OptionExample1.rs
@@ -1,6 +1,6 @@
-let fizzFor3 = Some(String::new()) // multiple of 3
-let buzzFor3 = None         // not multiple of 5
-let fizzbuzzFor3 = Some("") // fizzed ""
+let fizzFor3     = Some(String::new()) // multiple of 3
+let buzzFor3     = None                // not multiple of 5
+let fizzbuzzFor3 = Some(String::new()) // fizzed ""
 
 let fizzFor2     = None     // not multiple of 3
 let buzzFor2     = None     // not multiple of 5
