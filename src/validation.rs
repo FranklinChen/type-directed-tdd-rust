33a9229ffda9fc10ea6f0e71cb97c57523b590c8
Franklin Chen <franklinchen@franklinchen.com>
2015-07-26 21:03:07 -0400

    Relax Fn to FnOnce.

--- src/validation.rs
+++ src/validation.rs
@@ -19,7 +19,7 @@ pub fn single<T, E>(result: Result<T, E>) -> Validation<T, E> {
 pub fn add_with<V, T, U, E, F>(result1: Validation<V, E>,
                                result2: Validation<T, E>,
                                f: F) -> Validation<U, E>
-  where F: Fn(V, T) -> U
+  where F: FnOnce(V, T) -> U
 {
   match (result1, result2) {
     (Ok(v),       Ok(t))   => Ok(f(v, t)),
